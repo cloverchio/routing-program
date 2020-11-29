589a69cc61221e13de30e8d59924caaae913493d
chrisloverchio <chrisloverchio@gmail.com>
2020-11-29 03:36:20 -0700

    hashtable functionality

--- hashtable.py
+++ hashtable.py
@@ -8,35 +8,72 @@ class HashTable:
         self.load_factor = load_factor
         self.size = 0
         for i in range(0, capacity):
-            self.table[i] = []
+            self.table.append([])
 
     def __len__(self):
         return self.size
 
-    def insert(self, key, value):
+    def get(self, key):
+        """
+        Retrieves the value associated with the key from the hashtable.
+        :param key: in which to retrieve the value for.
+        :return: value associated with the key.
+        """
+        bucket_index = self._hash(key)
+        bucket = self.table[bucket_index]
+        if bucket:
+            for entry in bucket:
+                if entry[0] == key:
+                    return entry[1]
+        return None
+
+    def add(self, key, value):
+        """
+        Adds an entry to the hashtable using the given key/value pair.
+        :param key: to associate the value with.
+        :param value: in which to retrieve using the key.
+        :return:
+        """
+        if not key:
+            raise TypeError("key must not be none")
         self._increment_size()
         bucket_index = self._hash(key)
         bucket_value = (key, value)
         bucket = self.table[bucket_index]
         if bucket is None:
             self.table[bucket_index] = list(bucket_value)
-            return value
         else:
             for i in range(0, len(bucket)):
                 if bucket[i][0] == key:
                     bucket[i] = bucket_value
-                    return value
-            self.table[bucket_index].append(bucket_value)
-            return value
+                    return
+            bucket.append(bucket_value)
+
+    def remove(self, key):
+        """
+        Removes the entry associated with the key from the hashtable.
+        :param key: in which to remove the entry for.
+        :return:
+        """
+        bucket_index = self._hash(key)
+        bucket = self.table[bucket_index]
+        if bucket:
+            for i in range(0, len(bucket)):
+                if bucket[i][0] == key:
+                    bucket.pop(i)
+                    self._decrement_size()
 
     def _hash(self, key):
         return hash(key) % len(self.table)
 
     def _increment_size(self):
-        threshold = math.ceil(len(self.table) / self.load_factor)
-        if self.size + 1 > threshold:
+        # resize if threshold is violated
+        if self.size + 1 >= math.ceil(len(self.table) * self.load_factor):
             self._resize()
         self.size += 1
+
+    def _decrement_size(self):
+        self.size -= 1
 
     def _resize(self):
         table_len = len(self.table)
